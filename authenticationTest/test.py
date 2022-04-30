d61d0b0449ed523cd505f931d7d2f239d57a3a3f
Joe Mathew Konathapally <joe.konathapally@gmail.com>
2022-04-30 13:37:31 +0100

    Working python request struture

--- authenticationTest/test.py
+++ authenticationTest/test.py
@@ -2,15 +2,15 @@
 from email.quoprimime import body_check
 import requests
 #query = {'lat':'45', 'lon':'180'}
-email="something"
+email="jamesBond@gmail.com"
 # response = requests.post('https://httpbin.org/post', data = {'Email':email})
-response = requests.post('http://localhost:2000/findUserByEmail', body = {'Email':email})
+response = requests.post('http://localhost:2000/findUserByEmail', data = {'Email':email})
 # Update an existing resource
 # requests.put('https://httpbin.org/put', data = {'key':'value'})
 
 # print(response.headers["date"]) 
 
-print(response)
+print(response.text)
 
 #response.content() # Return the raw bytes of the data payload
 #response.text() # Return a string representation of the data payload
