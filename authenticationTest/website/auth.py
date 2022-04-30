97ab4657f2a0ce2ff06b65ee04c04fe4a8b6dc2d
Nick Omelianenko <nickomelianenko@Nicks-MacBook-Pro.local>
2022-04-30 15:10:08 +0100

    updated version of the authentication service

--- authenticationTest/website/auth.py
+++ authenticationTest/website/auth.py
@@ -1,12 +1,36 @@
+from lib2to3.pgen2.token import EQUAL
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
-from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+#from . import db
 from flask_login import login_user, login_required, logout_user, current_user
-
-
+import requests
+import json
+import jwt
 auth = Blueprint('auth', __name__)
+print("start")
 
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+
+        token = None
+
+        if 'x-access-tokens' in request.headers:
+            token = request.headers['x-access-tokens']
+
+        if not token:
+            return jsonify({'message': 'a valid token is missing'})
+        
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            current_user = Users.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({'message': 'token is invalid'})
+
+        return f(current_user, *args, **kwargs)
+    return decorator
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -14,11 +38,18 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
 
-        user = User.query.filter_by(email=email).first()
-        if user:
-            if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
-                login_user(user, remember=True)
+        #user = User.query.filter_by(email=email).first()
+        user = requests.post('http://localhost:2000/findUserByEmail', data = {'Email':email}).json()
+        print(user[0]["Password"])
+        print(len(user))
+        if not len(user)==0:
+            new_user = User(id=user[0]["UserID"], username=user[0]["UserName"], password=user[0]["Password"], email=user[0]["Email"])
+            db.session.add(new_user)
+            db.session.commit()
+            if user[0]["Password"]==password:
+            #     flash('Logged in successfully!', category='success')
+            #     print("User exists",email)
+                login_user(new_user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
