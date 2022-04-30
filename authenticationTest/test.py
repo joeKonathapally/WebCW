39d0d93ba9f5bd0412aac0555d4229fc64a892b5
Nick Omelianenko <nickomelianenko@Nicks-MacBook-Pro.local>
2022-04-30 13:27:45 +0100

    python test request

---
+++ authenticationTest/test.py
@@ -0,0 +1,18 @@
+
+from email.quoprimime import body_check
+import requests
+#query = {'lat':'45', 'lon':'180'}
+email="something"
+# response = requests.post('https://httpbin.org/post', data = {'Email':email})
+response = requests.post('http://localhost:2000/findUserByEmail', body = {'Email':email})
+# Update an existing resource
+# requests.put('https://httpbin.org/put', data = {'key':'value'})
+
+# print(response.headers["date"]) 
+
+print(response)
+
+#response.content() # Return the raw bytes of the data payload
+#response.text() # Return a string representation of the data payload
+#response.json()
+#print(response)
